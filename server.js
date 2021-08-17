becb95a48abb8a0931f77c29adef00b07116d396
ceblakely <cblakelydev@gmail.com>
2021-08-17 17:16:42 -0500

    Commit 2

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const flash = require("express-flash");
 const port = 3000;
-
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
-//app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, "/views")));
 
 app.get("/", (req, res) => {
-  res.render("home", { name: "Christina!!" });
+  res.render("home");
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -23,5 +19,4 @@ app.get("/menu", (req, res) => {
 app.get("/reservation", (req, res) => {
   res.render("reservation");
 });
-
 app.listen(port);
